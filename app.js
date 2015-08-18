cca59b6d610bb1cd27c24f6082539bcd0ae90da0
goofiw <goofiw@gmail.com>
2015-08-18 12:07:22 -0700

    added gitingore

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// var r = require('rethinkdb');
+var Firebase = require('firebase');
+var FirebaseTokenGenerator = require('firebase-token-generator');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +15,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,6 +23,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// var connection = null;
+// r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+//     if (err) throw err;
+//     connection = conn;
+// })
+
+// r.db('test').tableCreate('authors').run(connection, function(err, result) {
+//     if (err) throw err;
+//     console.log(JSON.stringify(result, null, 2));
+// })
+
+
+var tokenGenerator = new FirebaseTokenGenerator(secrets.FIREBASE_SECRET);
+var token = tokenGenerator.createToken({uid: "4", some: "randomdoberman", data: "ahhhh"});
+
+var dataRef = new Firebase('https://startuphall.firebaseio.com');
+dataRef.set("test");
 
 app.use('/', routes);
 app.use('/users', users);
